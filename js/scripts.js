7fc5e30d3d29daa41547b44f54459aab674882fa
dominic montelongo and Susan Kralian <dominic@>
2019-08-20 16:31:31 -0700

    resolve bugs

--- js/scripts.js
+++ js/scripts.js
@@ -7,52 +7,21 @@ $(document).ready(function(){
   $("#stressSurvey").submit(function(event){
     event.preventDefault();
 
-    var coping = parseInt($('.coping').val());
-    var symptom = parseInt($('.symptom').val());
-    console.log(coping);
-    console.log(symptom);
-    var score = symptom + coping
-    console.log(score);
-    $("#responses").show();
-    $("input:checkbox[name=symptom]:checked").each(function(){
-      console.log(symptom);
-      var stressResponse = $(this).val();
-
-    //   console.log(stressResponse);
-  })
-})
-});
-
-// part 2
-
-$(document).ready(function(){
-  $("form#fortune").submit(function(event){
-    event.preventDefault();
-    $(".hidden").hide();
-    var goodLuck = 0;
-    var badLuck = 0;
-    $("input:checkbox[name=badVibes]:checked").each(function(){
-
-      badLuck++;
+    var coping = 0;
+    var stress = 0;
+    $("input:checkbox[name=stress]:checked").each(function(){
+      stress++;
     });
-
-    $("input:checkbox[name=goodVibes]:checked").each(function(){
-      goodLuck++;
-
-    })
-      if(badLuck === goodLuck) {
-        $("#neutral").show();
-      } else if(badLuck >= goodLuck + 2) {
-        //check highest amount first
-          $("#badResponses2").show();
-      } else if(goodLuck >= badLuck +2) {
-          $("#goodResponses2").show();
-      } else if (badLuck > goodLuck) {
-          $("#badResponses1").show();
-      } else if (goodLuck > badLuck) {
-          $("#goodResponses1").show();
-      }
-
-
-  });
+    $("input:checkbox[name=coping]:checked").each(function(){
+      coping++;
+    });
+    if(stress===coping) {
+      $("#neutral").show();
+    } else if(coping > stress) {
+      //check highest amount first
+        $("#cope").show();
+    } else if(coping < stress) {
+        $("#die").show();
+    };
+        });
 });
